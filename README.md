2ab864be1cf2516fd67c37f26b0a472a163a48e3
c-poteat <nellp7@yahoo.com>
2023-01-07 19:36:50 -0500

    Completed scaping and image transfer through email

---
+++ README.md
@@ -0,0 +1,29 @@
+# PowerShell Scraper
+
+## Description
+
+This project was created to scape motivational quote images from a website under the .png file extension.
+
+## Getting Started
+
+### Installing
+
+* git clone file and use VSCode to run the file.
+
+### Executing program
+
+Download VSCode and run the function by typing the file name in the terminal. Make sure you are in the current working directory.
+
+## Authors
+
+Carnell
+
+## Version History
+
+* 1.0
+
+
+## Acknowledgments
+
+* www.google.com
+* https://learn.microsoft.com/en-us/powershell/
---
+++ src/webscraper.ps1
@@ -0,0 +1,31 @@
+
+function webscraper {
+$html = Invoke-WebRequest -Uri 'https://bqotd.com/'
+$htmlContent = $html.Content
+$imageUrls = Select-String -InputObject $htmlContent -Pattern '<img src="([^"]+png)"' -AllMatches | % { $_.Matches } | % { $_.Groups[1].Value }
+foreach ($imageUrl in $imageUrls) {
+    Invoke-WebRequest -Uri $imageUrl -OutFile "$((Split-Path -Leaf $imageUrl))" #Outfile location is the current active directory.
+  }
+}
+
+function sendemailwithpic {
+$from = "someone@mail.com" #used mail.com setting since gmail and yahoo mail do not allow less secure connections
+$to = "someone@gmail.com"  #replace with actual gmail address
+$subject = "A great motivating quote while using PowerShell"
+$body = "Quote of the Day"
+$directory = "/Users/nell/Documents/DEV/PowerShell/webscraper-ps"
+$imageExtensions = @("*.jpg", "*.png", "*.gif")
+$attachment = Get-ChildItem -Path $directory -Include $imageExtensions -Recurse | Get-Random
+$username = "yourusername@mail.com"  #again signed up to mail.com
+$password = "yourpassword"
+Send-MailMessage -From $from -To $to -Subject $subject -Body $body -Attachment $attachment -SmtpServer smtp.mail.com -Port 587 -UseSsl -Credential (New-Object System.Management.Automation.PSCredential($username,(ConvertTo-SecureString $password -AsPlainText -Force)))
+}
+
+function main {
+  webscraper
+  sendemailwithpic
+}
+
+
+
+
