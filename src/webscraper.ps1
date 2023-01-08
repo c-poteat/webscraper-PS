0c86c7f537240c9616d7dc1b7d6bd7e18202f308
c-poteat <nellp7@yahoo.com>
2023-01-07 19:46:03 -0500

    Completed scripts

--- src/webscraper.ps1
+++ src/webscraper.ps1
@@ -7,23 +7,25 @@ foreach ($imageUrl in $imageUrls) {
     Invoke-WebRequest -Uri $imageUrl -OutFile "$((Split-Path -Leaf $imageUrl))" #Outfile location is the current active directory.
   }
 }
-
 function sendemailwithpic {
-$from = "someone@mail.com" #used mail.com setting since gmail and yahoo mail do not allow less secure connections
-$to = "someone@gmail.com"  #replace with actual gmail address
+$from = "youremail@mail.com" #used mail.com setting since gmail and yahoo mail do not allow less secure connections
+$to = "youremail@gmail.com"  #replace with actual gmail address
 $subject = "A great motivating quote while using PowerShell"
 $body = "Quote of the Day"
 $directory = "/Users/nell/Documents/DEV/PowerShell/webscraper-ps"
 $imageExtensions = @("*.jpg", "*.png", "*.gif")
 $attachment = Get-ChildItem -Path $directory -Include $imageExtensions -Recurse | Get-Random
-$username = "yourusername@mail.com"  #again signed up to mail.com
+$username = "youremail@mail.com"  #again signed up to mail.com
 $password = "yourpassword"
 Send-MailMessage -From $from -To $to -Subject $subject -Body $body -Attachment $attachment -SmtpServer smtp.mail.com -Port 587 -UseSsl -Credential (New-Object System.Management.Automation.PSCredential($username,(ConvertTo-SecureString $password -AsPlainText -Force)))
+
 }
 
 function main {
   webscraper
   sendemailwithpic
+
+  Write-Output "Email was sent successfully"
 }
 
 
